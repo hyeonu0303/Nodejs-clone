768156a9c532089216f300efadcbe9849466a6b7
Parkhyeonwoo <hyeonu030334@gmail.com>
2023-07-06 16:07:38 +0900

    npm->pnpm변경 및 회원가입기능구현

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const MongoClient = require('mongodb').MongoClient;
 app.set('view engine','ejs')
 app.use(express.urlencoded({extended : true}))
 app.use(express.json());
+//login 라이브러리
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 
+app.use(session({secret : '비밀코드', resave : true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session()); 
+//method-override
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
 
@@ -55,11 +63,12 @@ app.get('/list',(req,res)=>{
 app.delete('/delete',(req,res)=>{
   //_id가 str로 받아와져서 이렇게됨
   //req.body._id = parseInt(req.body._id)
-  let id = parseInt(req.params.id);
-  console.log(id)
-  db.collection('post').deleteOne(id,(error,result)=>{
-    if(error) console.log('삭제요청:'+error)
-    res.send('삭제완료')
+  req.body._id = parseInt(req.body._id);
+  db.collection('post').deleteOne(req.body,(error,result)=>{
+    db.collection('counter').updateOne({name:'게시물갯수'},{$inc : {totalPost: -1}},(error,result)=>{
+      if(error) console.log('삭제요청:'+error)
+      res.send('삭제완료')
+    })
   })
 })
 
@@ -73,7 +82,6 @@ app.get('/detail/:id',(req,res)=>{
 //edit.ejs GET요청처리
 app.get('/edit/:id',(req,res)=>{
   //제목과 date값을 찾아서 data로 전달해줘야함 
-  //parseInt(req.params.id)
   db.collection('post').findOne({_id : parseInt(req.params.id)},(error,result)=>{
     if(error) console.log(error)
     console.log(result)
@@ -91,5 +99,70 @@ app.put('/edit',(req,res)=>{
   })
 })
 
+//login
+app.get('/login',(req,res)=>{
+  res.render('login.ejs')
+})
 
+app.post('/login', passport.authenticate('local', {failureRedirect : '/fail'}), function(요청, 응답){
+  응답.redirect('/')
+});
 
+passport.use(new LocalStrategy({
+  usernameField: 'id', //(요기는 사용자가 제출한 아이디가 어디 적혔는지) 
+    passwordField: 'pw', //(요기는 사용자가 제출한 비번이 어디 적혔는지) 
+    session: true, //(요기는 세션을 만들건지) 
+    passReqToCallback: false, //(요기는 아이디/비번말고 다른 정보검사가 필요한지) 
+}, function (입력한아이디, 입력한비번, done) {
+  //console.log(입력한아이디, 입력한비번);
+  //아이디/비번을 DB데이터와 비교
+  db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+    if (에러) return done(에러)
+
+    if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+    if (입력한비번 == 결과.pw) {
+      return done(null, 결과)
+    } else {
+      return done(null, false, { message: '비번틀렸어요' })
+    }
+  })
+}));
+
+//세션만들고 세션아이디 발급해서 쿠키로 보내주기
+passport.serializeUser(function (user, done) {
+  done(null, user.id)
+});
+
+//로그인한 유저의 개인정보를 DB에서 찾는 역할
+passport.deserializeUser(function (아이디, done) {
+  //디비에서 위에있는 user.id를 유저를 찾은 뒤에 유저정보를 밑에넣음 아이디 == user.id
+  db.collection('login').findOne({id:아이디},(error,result)=>{
+    done(null, result)
+  })
+}); 
+
+//로그인 미들웨어(로그인했는지안했는지)
+let 로그인했니 = (req,res,next)=>{
+  if(req.user){
+    next()
+  }else{
+    res.send('로그인 하셔야될듯요')
+  }
+}
+//mypage
+app.get('/mypage',로그인했니,(req,res)=>{
+    console.log(req.user)
+    res.render('mypage.ejs',{user : req.user}) 
+})
+//회원가입페이지
+app.get('/register',(req,res)=>{
+  res.render('register.ejs')
+})
+
+app.post('/register',(req,res)=>{
+  db.collection('login').insertOne({id:req.body.id,pw:req.body.pw},(error,result)=>{
+    if(error) console.log(error)
+    console.log('회원가입완료')
+    res.redirect('/login')
+  })
+})
