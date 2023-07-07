1280a2ac31988eda5efc4e3956fdcdab6666b9f9
Parkhyeonwoo <hyeonu030334@gmail.com>
2023-07-08 00:52:47 +0900

    삭제요청 작성자 확인추가

--- server.js
+++ server.js
@@ -14,6 +14,48 @@ app.use(passport.session());
 //method-override
 const methodOverride = require('method-override')
 app.use(methodOverride('_method'))
+
+
+passport.use(new LocalStrategy({
+  usernameField: 'id', //(요기는 사용자가 제출한 아이디가 어디 적혔는지) 
+    passwordField: 'pw', //(요기는 사용자가 제출한 비번이 어디 적혔는지) 
+    session: true, //(요기는 세션을 만들건지) 
+    passReqToCallback: false, //(요기는 아이디/비번말고 다른 정보검사가 필요한지) 
+}, function (입력한아이디, 입력한비번, done) {
+  //console.log(입력한아이디, 입력한비번);
+  //아이디/비번을 DB데이터와 비교
+  db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
+    if (에러) return done(에러)
+
+    if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
+    if (입력한비번 == 결과.pw) {
+      return done(null, 결과)
+    } else {
+      return done(null, false, { message: '비번틀렸어요' })
+    }
+  })
+}));
+
+//세션만들고 세션아이디 발급해서 쿠키로 보내주기
+passport.serializeUser(function (user, done) {
+  done(null, user.id)
+});
+
+//로그인한 유저의 개인정보를 DB에서 찾는 역할
+passport.deserializeUser(function (아이디, done) {
+  //디비에서 위에있는 user.id를 유저를 찾은 뒤에 유저정보를 밑에넣음 아이디 == user.id
+  db.collection('login').findOne({id:아이디},(error,result)=>{
+    done(null, result)
+  })
+});
+//로그인 미들웨어(로그인했는지안했는지)
+let 로그인했니 = (req,res,next)=>{
+  if(req.user){
+    next()
+  }else{
+    res.send('로그인 하셔야될듯요')
+  }
+} 
 //dotenv
 require('dotenv').config()
 app.get('/', (req, res) => {
@@ -50,7 +92,7 @@ app.delete('/delete',(req,res)=>{
   //_id가 str로 받아와져서 이렇게됨
   //req.body._id = parseInt(req.body._id)
   req.body._id = parseInt(req.body._id);
-  db.collection('post').deleteOne(req.body,(error,result)=>{
+  db.collection('post').deleteOne({_id:req.body._id, 작성자 : req.user._id},(error,result)=>{
     db.collection('counter').updateOne({name:'게시물갯수'},{$inc : {totalPost: -1}},(error,result)=>{
       if(error) console.log('삭제요청:'+error)
       res.send('삭제완료')
@@ -94,47 +136,6 @@ app.post('/login', passport.authenticate('local', {failureRedirect : '/fail'}), 
   응답.redirect('/')
 });
 
-passport.use(new LocalStrategy({
-  usernameField: 'id', //(요기는 사용자가 제출한 아이디가 어디 적혔는지) 
-    passwordField: 'pw', //(요기는 사용자가 제출한 비번이 어디 적혔는지) 
-    session: true, //(요기는 세션을 만들건지) 
-    passReqToCallback: false, //(요기는 아이디/비번말고 다른 정보검사가 필요한지) 
-}, function (입력한아이디, 입력한비번, done) {
-  //console.log(입력한아이디, 입력한비번);
-  //아이디/비번을 DB데이터와 비교
-  db.collection('login').findOne({ id: 입력한아이디 }, function (에러, 결과) {
-    if (에러) return done(에러)
-
-    if (!결과) return done(null, false, { message: '존재하지않는 아이디요' })
-    if (입력한비번 == 결과.pw) {
-      return done(null, 결과)
-    } else {
-      return done(null, false, { message: '비번틀렸어요' })
-    }
-  })
-}));
-
-//세션만들고 세션아이디 발급해서 쿠키로 보내주기
-passport.serializeUser(function (user, done) {
-  done(null, user.id)
-});
-
-//로그인한 유저의 개인정보를 DB에서 찾는 역할
-passport.deserializeUser(function (아이디, done) {
-  //디비에서 위에있는 user.id를 유저를 찾은 뒤에 유저정보를 밑에넣음 아이디 == user.id
-  db.collection('login').findOne({id:아이디},(error,result)=>{
-    done(null, result)
-  })
-}); 
-
-//로그인 미들웨어(로그인했는지안했는지)
-let 로그인했니 = (req,res,next)=>{
-  if(req.user){
-    next()
-  }else{
-    res.send('로그인 하셔야될듯요')
-  }
-}
 //mypage
 app.get('/mypage',로그인했니,(req,res)=>{
     console.log(req.user)
@@ -181,10 +182,10 @@ app.post('/register',(req,res)=>{
   })
 })
 
-app.post('/add',로그인했니,(req,res)=>{
+app.post('/add',(req,res)=>{
   db.collection('counter').findOne({name: '게시물갯수'}, (error,result)=>{
     let totalPost = result.totalPost
-    db.collection('post').insertOne({_id:(totalPost + 1),작성자:req.user._id,title:req.body.title,date:req.body.date},(error,result)=>{
+    db.collection('post').insertOne({_id:(totalPost + 1),작성자:req.user._id,title:req.body.title,date:req.newDate()},(error,result)=>{
       console.log(result)
       db.collection('counter').updateOne({name:'게시물갯수'},{$inc : {totalPost:1}},(error,result)=>{
         if(error) console.log(error)
